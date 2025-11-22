1932f5d2d8263d3c0a11abbec8ee54d9a63c7f83
SonamYangchen1177 <sooyanger@gmail.com>
2025-11-22 16:53:56 +0600

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import Input, Concatenate, Dense, Dropout, BatchNormalization, LeakyReLU
 from tensorflow.keras.models import Model
